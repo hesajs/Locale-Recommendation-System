4ec2ec129a3304349822b02484f98357f1fe9503
sakxyam adhikari <sakxyamadk@gmail.com>
2021-12-12 08:49:51 +0545

    Model Integration

---
+++ LRS_WEB/Detection/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from Detection.predict import Predict
+# Create your views here.
+
+
+p = Predict()
+def detect(request):
+    if(request.method == 'POST'):
+        img = request.FILES.get('img')
+        result = p.predictHTMLDirect(img)        
+        return render(request, 'index.html',{"var":"The scene was identified as " + str(result)})
+    else:
+        return render(request,'index.html',{"var":"Select Image"})
