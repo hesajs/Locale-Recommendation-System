4c81533d0b7fa8f842b08379c706d04812f6538f
Sajesh Shrestha <shresthasajs@gmail.com>
2022-03-29 17:21:48 +0545

    Fix Image persistance after submission

--- LRS_WEB/Detection/views.py
+++ LRS_WEB/Detection/views.py
@@ -1,16 +1,24 @@
+import base64
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from Detection.predict import Predict
+
 # Create your views here.
 
 
 p = Predict()
 def detect(request):
     if(request.method == 'POST'):
+        import pdb 
+        pdb.set_trace()
         img = request.FILES.get('img')
         result = p.predictHTMLDirect(img)
         request.session['var'] = str(result)
-        return render(request, 'index.html',{"var":str(result), "img":img})
+        img = request._files.get('img')
+        with img.open("rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+            decoded_string = str(encoded_string, "utf-8")
+        return render(request, 'index.html',{"var":str(result), "img":decoded_string})
     else:
         return render(request,'index.html',{"var":""})
 
